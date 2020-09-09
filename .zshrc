c37ae07499c48a11b036dad24904b01ed3e1da2a
Ben <me@bpmct.net>
2020-09-09 17:55:51 +0000

    zsh preferences

---
+++ .zshrc
@@ -0,0 +1,12 @@
+export ZSH=$HOME/.oh-my-zsh
+export ZSH_THEME="zanshin"
+
+plugins=(
+  git
+  bundler
+  dotenv
+  osx
+  rake
+  rbenv
+  ruby
+)
---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Install ZSH
+sudo apt-get install -y vim zsh
+sudo chsh -s $(readlink -f $(which zsh)) $USER
+
+# Install Oh My ZSH
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
