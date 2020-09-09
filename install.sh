b8faf5d28d402538bbd0cdaa4190f78b27e4a882
Ben <me@bpmct.net>
2020-09-09 18:25:28 +0000

    zsh unattended install

--- install.sh
+++ install.sh
@@ -5,4 +5,4 @@ sudo apt-get install -y vim zsh
 sudo chsh -s $(readlink -f $(which zsh)) $USER
 
 # Install Oh My ZSH
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
