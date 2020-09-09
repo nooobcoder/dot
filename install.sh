c661c72409f862becb69e1e0a274d3a0f988f7be
Ben <me@bpmct.net>
2020-09-09 18:34:17 +0000

    change ZSH install

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 # Install ZSH
 sudo apt-get install -y vim zsh
-sudo chsh -s $(readlink -f $(which zsh)) $USER
+sudo chsh -s /bin/zsh $USER
 
 # Install Oh My ZSH
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
