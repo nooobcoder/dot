e9239351f2950155aeef0c8121b873b84251c9a2
Ben <me@bpmct.net>
2021-02-14 19:15:08 +0000

    only do vscode stuff once

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ sudo chsh -s /bin/zsh $USER
 # Install and configure Oh My ZSH (if it is not already installed)
 if [ -d "~/.oh-my-zsh" ] 
 then
-    echo "Directory /path/to/dir exists." 
+    echo "oh-my-zsh is already installed" 
 else
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
     # Append zshrc stuff to end of file
@@ -18,8 +18,15 @@ fi
 # copy dotfiles into ~
 shopt -s dotglob # include . in *
 shopt -s extglob
-yes | cp -rf ~/dotfiles/!(.git|.zshrc|.|..) ~
+yes | cp -rf ~/dotfiles/!(.git|.zshrc|.|..|.local) ~
 
-# Install extensions
-/opt/coder/code-server/bin/code-server --install-extension esbenp.prettier-vscode
-/opt/coder/code-server/bin/code-server daylerees.rainglow
+# Set VS Code preferences for the FIRST time
+if [ -f "~/.local/share/code-server/User/settings.json" ] 
+then
+    echo "VS Code settings are already present." 
+else
+    cp -rf dotfiles/.local ~/local
+    # Install extensions
+    /opt/coder/code-server/bin/code-server --install-extension esbenp.prettier-vscode
+    /opt/coder/code-server/bin/code-server daylerees.rainglow
+fi
